c51bfc8110c3f96189f49a7a20dd1b942174df8c
nguhero <phamvanngu647@gmail.com>
2019-07-17 10:50:14 +0700

    commit lan 4

--- src/nguhero/Hello.java
+++ src/nguhero/Hello.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Xin chao ban Pham Van Ngu lan 2");
 	System.out.println("Xin chao ban Pham Van Ngu lan 3");
 	System.out.println("Xin chao ban Pham Van Ngu lan 4");
+	System.out.println("Xin chao ban Pham Van Ngu lan 5");
 }
 }
